60267b3d725b7a3202667211d6cdb24a2e8cd109
Martin Gruber <gruber95@ads.uni-passau.de>
2023-11-14 11:46:25 +0100

    Extended the functionality a bit

--- visualizingDebates/app/graph/graph_builder_x.py
+++ visualizingDebates/app/graph/graph_builder_x.py
@@ -22,7 +22,20 @@ def extract_file(json_file_path):
         graph_data = json.load(json_file)
 
         for node in graph_data["nodes"]:
-            graph.add_node(node["nodeID"], text=node["text"], type=node["type"])
+            node_id = node["nodeID"]
+            text = node["text"]
+            node_type = node["type"]
+
+            # Check if there is a matching locution for the node
+            matching_locution = next(
+                (locution for locution in graph_data["locutions"] if locution["nodeID"] == node_id), None)
+
+            if matching_locution:
+                start_time = matching_locution.get("start")
+                speaker = matching_locution.get("personID")
+                graph.add_node(node_id, text=text, type=node_type, start=start_time, speaker=speaker)
+            else:
+                graph.add_node(node_id, text=text, type=node_type)
 
         for edge in graph_data["edges"]:
             graph.add_edge(edge["fromID"], edge["toID"])
@@ -31,74 +44,10 @@ def extract_file(json_file_path):
 def remove_isolated(graph):
     nodes_to_remove = [node for node, data in graph.nodes(data=True) if
                        data["type"] == "L" and graph.degree(node) == 0]
-
     for node in nodes_to_remove:
         graph.remove_node(node)
 
-
 def collapse_edges(graph):
-    i_nodes_to_update = set()
-    l_nodes_to_update = set()
-
-    # Identify "I" nodes connected to other "I" nodes via "RA", "MA", or "CA" nodes
-    for node, attributes in graph.nodes(data=True):
-        if attributes["type"] == "I":
-            neighbors = set(graph.neighbors(node))
-            additional_info_neighbors = {n for n in neighbors if graph.nodes[n]["type"] in {"RA", "MA", "CA"}}
-            i_neighbors = {n for additional_info_node in additional_info_neighbors for n in
-                           graph.neighbors(additional_info_node)}
-            if i_neighbors:
-                i_nodes_to_update.add(node)
-        elif attributes["type"] == "L":
-            neighbors = set(graph.neighbors(node))
-            additional_info_neighbors = {n for n in neighbors if graph.nodes[n]["type"] == "TA"}
-            l_neighbors = {n for additional_info_node in additional_info_neighbors for n in
-                           graph.neighbors(additional_info_node)}
-            if l_neighbors:
-                l_nodes_to_update.add(node)
-
-    # Create a list to store edges and nodes to remove
-    edges_to_remove = []
-    nodes_to_remove = []
-
-    # Update "I" to "I" edges
-    for i_node in i_nodes_to_update:
-        neighbors = set(graph.neighbors(i_node))
-        additional_info_neighbors = {n for n in neighbors if graph.nodes[n]["type"] in {"RA", "MA", "CA"}}
-
-        # Iterate over "RA", "MA", or "CA" nodes connected to the "I" node
-        for additional_info_node in additional_info_neighbors:
-            i_neighbors = {n for n in graph.neighbors(additional_info_node) if graph.nodes[n]["type"] == "I"}
-            for i_neighbor in i_neighbors:
-                # Replace the old edge with a new edge containing additional information
-                graph.add_edge(i_node, i_neighbor, text_additional=graph.nodes[additional_info_node]["text"])
-
-                # Add the old edge and node to the removal lists
-                edges_to_remove.append((additional_info_node, i_neighbor))
-                nodes_to_remove.append(additional_info_node)
-    for l_node in l_nodes_to_update:
-        neighbors = set(graph.neighbors(l_node))
-        additional_info_neighbors = {n for n in neighbors if graph.nodes[n]["type"] == "TA"}
-
-        # Iterate over "TA" nodes connected to the "L" node
-        for additional_info_node in additional_info_neighbors:
-            l_neighbors = {n for n in graph.neighbors(additional_info_node) if graph.nodes[n]["type"] == "L"}
-            for l_neighbor in l_neighbors:
-                # Replace the old edge with a new edge containing additional information
-                graph.add_edge(l_node, l_neighbor, text_additional=graph.nodes[additional_info_node]["text"])
-
-                # Add the old edge and node to the removal lists
-                edges_to_remove.append((additional_info_node, l_neighbor))
-                nodes_to_remove.append(additional_info_node)
-
-    # Remove the old edges and nodes
-    for edge_to_remove in edges_to_remove:
-        graph.remove_edge(*edge_to_remove)
-    for node_to_remove in nodes_to_remove:
-        graph.remove_node(node_to_remove)
-
-
-def collapse_edgesX(graph):
     l_nodes_to_update = set()
 
     # Identify "L" nodes connected to other "L" nodes via "TA", "RA", "MA", or "CA" nodes
@@ -170,8 +119,7 @@ def collapse_nodes(graph):
                 i_node = i_nodes[0]
 
                 # Create a new node with the attributes of the L node
-                graph.add_node(l_node, text=graph.nodes[l_node]["text"], paraphrasedtext=graph.nodes[i_node]["text"],
-                               type="L")
+                graph.add_node(l_node, paraphrasedtext=graph.nodes[i_node]["text"])
 
                 # Add edges from "L" and "I" nodes to the new node
                 for neighbor in set(graph.neighbors(l_node)) | set(graph.neighbors(i_node)):
@@ -191,15 +139,14 @@ def collapse_nodes(graph):
 
 extract_file(None)
 remove_isolated(graph)
-# collapse_edges(graph)
 collapse_nodes(graph)
-collapse_edgesX(graph)
+collapse_edges(graph)
 
 for node_id, attributes in graph.nodes(data=True):
     print(f"Node {node_id}: {attributes}")
 # print(graph.edges(data=True))
-for edge in graph.edges(data=True):
-    source, target, data = edge
-    print(f"Edge: {graph.nodes[source]['paraphrasedtext']} -- {graph.nodes[target]['text']}")
-    print(f"Additional Information: {data.get('text_additional', 'No additional information')}, {data.get('connType')}")
-    print("---")
+#for edge in graph.edges(data=True):
+#    source, target, data = edge
+#    print(f"Edge: {graph.nodes[source]['paraphrasedtext']} -- {graph.nodes[target]['text']}")
+#    print(f"Additional Information: {data.get('text_additional', 'No additional information')}, {data.get('connType')}")
+#    print("---")
