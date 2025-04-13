7f1bec19238c376ab27e41707c591a576da3c0b4
Martin Gruber <gruber95@ads.uni-passau.de>
2025-04-13 12:03:51 +0200

    Changed welcome page and added a folder containing the (current) analysis results in order to be able to inspect it without running the server

--- visualizingDebates/app/graph/__init__.py
+++ visualizingDebates/app/graph/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import json
+from datetime import datetime
 
 from .topic_extraction import extract_topics
 from .data_extraction import extract_data
@@ -12,6 +13,19 @@ topics = extract_topics()
 graph_data["topics"] = topics
 logging.info("Finished topic extraction")
 
-# save results into a file
-with open("analysis_reults/analysis_results.json", "w") as f:
+# save results into a JSON file
+with open("analysis_results/analysis_results.json", "w") as f:
     f.write(json.dumps(graph_data, default=str))
+
+
+def convert(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+
+# Convert and save as a JS file for local (serverless) inspection via opening the html in the results folder
+with open("analysis_results/graph_data_test.js", "w") as f:
+    f.write("const graphData = ")
+    json.dump(graph_data, f, default=convert)
+    f.write(";")
